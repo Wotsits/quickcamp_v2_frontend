0b15349a424114126fab9ecbacaab1a7eb2da275
Simon Sexton <simon.sexton@live.co.uk>
2023-09-13 20:37:12 +0100

    styling: MUI-ify the booking summery popover

--- src/components/ResourceCalendar/BookingSummary.tsx
+++ src/components/ResourceCalendar/BookingSummary.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Booking } from "./types";
+import { Card, CardActions, CardContent, CardHeader, IconButton } from "@mui/material";
+import LockIcon from '@mui/icons-material/Lock';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 type BookingSummaryProps = {
   /** mandatory, booking object */
@@ -16,41 +19,58 @@ const BookingSummary = ({
   positionTop,
 }: BookingSummaryProps) => {
   return (
-    <div
+    <Card
+      variant="outlined"
       style={{ position: "absolute", left: positionLeft, top: positionTop }}
       className="booking-summary"
     >
+      <CardHeader
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={booking.bookingName}
+        subheader={`Ref: ${booking.id}`}
+      />
 
-      <div className="booking-summary-header">
-        <span className="booking-name">{booking.bookingName}</span>
-        <span>Ref: {booking.id}</span>
-      </div>
+      <CardContent>
+        <div className="booking-summary-header">
+          <span className="booking-name">{booking.bookingName}</span>
+          <span>Ref: {booking.id}</span>
+        </div>
 
-      <div className="booking-summary-stay-details">
-        <div>Unit: {booking.unit}</div>
-        <div>Start: {new Date(booking.start).toLocaleString()}</div>
-        <div>End: {new Date(booking.end).toLocaleString()}</div>
-      </div>
-      
-      <div className="booking-summary-occupant-details">
-        <div>Adults: {booking.adults}</div>
-        <div>Children: {booking.children}</div>
-        <div>Infants: {booking.infants}</div>
-        <div>Pets: {booking.pets}</div>
-        <div>Vehicles: {booking.vehicles}</div>
-      </div>
-      
-      <div className="booking-summary-finance-details">
-        <div>Paid: {booking.paid ? "Yes" : "No"}</div>
-      </div>
-      
-      <div className="booking-summary-checkedin-details">
-        <div>People Checked In: {booking.peopleCheckedIn}</div>
-        <div>Pets Checked In: {booking.petsCheckedIn}</div>
-        <div>Vehicles Checked In: {booking.vehiclesCheckedIn}</div>
-      </div>
+        <div className="booking-summary-stay-details">
+          <div>Unit: {booking.unit}</div>
+          <div>Start: {new Date(booking.start).toLocaleString()}</div>
+          <div>End: {new Date(booking.end).toLocaleString()}</div>
+        </div>
 
-    </div>
+        <div className="booking-summary-occupant-details">
+          <div>Adults: {booking.adults}</div>
+          <div>Children: {booking.children}</div>
+          <div>Infants: {booking.infants}</div>
+          <div>Pets: {booking.pets}</div>
+          <div>Vehicles: {booking.vehicles}</div>
+        </div>
+
+        <div className="booking-summary-finance-details">
+          <div>Paid: {booking.paid ? "Yes" : "No"}</div>
+        </div>
+
+        <div className="booking-summary-checkedin-details">
+          <div>People Checked In: {booking.peopleCheckedIn}</div>
+          <div>Pets Checked In: {booking.petsCheckedIn}</div>
+          <div>Vehicles Checked In: {booking.vehiclesCheckedIn}</div>
+        </div>
+      </CardContent>
+
+      <CardActions disableSpacing>
+        <IconButton aria-label="lock">
+          <LockIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
   );
 };
 
