e77e7d46e16800c267d6ea5750afc56da5307e97
Simon Sexton <simon.sexton@live.co.uk>
2023-09-14 10:36:40 +0100

    refactor: extract router

--- src/index.tsx
+++ src/index.tsx
@@ -1,51 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import Root from "./routes/root";
-import ErrorPage from "./components/ErrorPage";
-import Dashboard from "./routes/dashboard";
-import BookingCalendar from "./routes/booking-calendar";
-import Guests from "./routes/guests";
-import Bookings from "./routes/bookings";
-import Admin from "./routes/admin";
-import { CssBaseline } from "@mui/material";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-        {
-            path: "dashboard/",
-            element: <Dashboard />,
-        },
-        {
-            path: "booking-calendar/",
-            element: <BookingCalendar />,
-        },
-        {
-            path: "guests/",
-            element: <Guests />,
-        },
-        {
-            path: "bookings/",
-            element: <Bookings />,
-        },
-        {
-            path: "admin/",
-            element: <Admin />,
-        },
-    ]
-  },
-]);
+import { CssBaseline } from "@mui/material";
+import { router } from "./router";
 
 const container = document.getElementById("app-root")!;
 const root = createRoot(container);
---
+++ src/router.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Root from './routes/root';
+import ErrorPage from './components/ErrorPage';
+import { Dashboard } from '@mui/icons-material';
+import BookingCalendar from './routes/booking-calendar';
+import Guests from './routes/guests';
+import Bookings from './routes/bookings';
+import Admin from './routes/admin';
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      errorElement: <ErrorPage />,
+      children: [
+          {
+              path: "dashboard/",
+              element: <Dashboard />,
+          },
+          {
+              path: "booking-calendar/",
+              element: <BookingCalendar />,
+          },
+          {
+              path: "guests/",
+              element: <Guests />,
+          },
+          {
+              path: "bookings/",
+              element: <Bookings />,
+          },
+          {
+              path: "admin/",
+              element: <Admin />,
+          },
+      ]
+    },
+  ]);
