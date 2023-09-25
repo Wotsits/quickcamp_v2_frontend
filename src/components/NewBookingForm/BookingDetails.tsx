4e3f55a8dd366876cfcf8a97ab95fed3a02400f8
Simon Sexton <simon.sexton@live.co.uk>
2023-09-25 02:51:14 +0100

    feat: placeholder for Booking Details

--- src/components/NewBookingForm/BookingDetails.tsx
+++ src/components/NewBookingForm/BookingDetails.tsx
@@ -1,10 +1,21 @@
-import { Typography } from "@mui/material";
+import { Box, InputLabel, Menu, MenuItem, Select, TextField, Typography } from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers";
 import React from "react";
 
 const BookingDetails = () => {
   return (
     <div id="booking-details">
-      Booking Details Page
+      <Box marginBottom={3}>
+        <div id="date-container">
+          <DatePicker label="Start Date" sx={{width: "100%"}} />
+          <DatePicker label="End Date" sx={{width: "100%"}} />
+        </div>
+        <TextField fullWidth label="Pitch" select>
+          <MenuItem value={1}>Pitch 1</MenuItem>
+          <MenuItem value={2}>Pitch 2</MenuItem>
+          <MenuItem value={3}>Pitch 3</MenuItem>
+        </TextField>
+      </Box>
     </div>
   );
 }
