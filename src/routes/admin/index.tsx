89eb1bc5eb541d9bceeb7cc94f2155bf3c59767a
Simon Sexton <simon.sexton@live.co.uk>
2023-12-21 19:02:15 +0000

    feat: remove links to superfluous items from admin (will be accessed via site page)

--- src/routes/admin/index.tsx
+++ src/routes/admin/index.tsx
@@ -15,51 +15,9 @@ const Admin = () => {
             Sites
           </Typography>
           <Typography variant="body1" gutterBottom>
-            Add, remove, and edit sites.
+            Add, remove, and edit sites, including their unit types, units, guest types, rates and availability.
           </Typography>
         </Link>
-        <Link to={ROUTES.ROOT + ROUTES.ADMIN + ROUTES.UNIT_TYPES} className="admin-content-item">
-          <Typography variant="h6" gutterBottom>
-            Unit Types
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            Add, remove, and edit Unit Types.
-          </Typography>
-        </Link>
-        <Link to={ROUTES.ROOT + ROUTES.ADMIN + ROUTES.UNITS} className="admin-content-item">
-          <Typography variant="h6" gutterBottom>
-            Units
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            Add, remove, and edit Units.
-          </Typography>
-        </Link>
-        <Link to={ROUTES.ROOT + ROUTES.ADMIN + ROUTES.GUEST_TYPES} className="admin-content-item">
-          <Typography variant="h6" gutterBottom>
-            Guest Types
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            Add, remove, and edit Guest Types.
-          </Typography>
-        </Link>
-        <Link to={ROUTES.ROOT + ROUTES.ADMIN + ROUTES.EQUIPMENT_TYPES} className="admin-content-item">
-          <Typography variant="h6" gutterBottom>
-            Equipment Types
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            Add, remove, and edit Equipment Types.
-          </Typography>
-        </Link>
-        <Link to={ROUTES.ROOT + ROUTES.ADMIN + ROUTES.EXTRA_TYPES} className="admin-content-item">
-          <Typography variant="h6" gutterBottom>
-            Extra Types
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            Add, remove, and edit Extra Types.
-          </Typography>
-        </Link>
-      </div>
-      <div className="admin-content-container">
         <Link to={ROUTES.ROOT + ROUTES.ADMIN + ROUTES.USERS} className="admin-content-item">
           <Typography variant="h6" gutterBottom>
             Users
